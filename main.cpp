2ba399db823d0b223eca6c7a1a28faf04406d4b7
yoonnixxx <pastuch@alunos.utfpr.edu.br>
2025-10-27 19:28:01 -0300

    config funcionando

--- main.cpp
+++ main.cpp
@@ -1,5 +1,26 @@
+#include <filesystem>
+#include <fstream>
 #include <iostream>
-int main(int argc, char** argv) {
-    std::cout << "Bootstrap OK" << std::endl;
+
+int main() {
+    std::filesystem::path path = std::filesystem::current_path() / "config_exemplo.txt";
+
+    std::cout << "CWD = " << std::filesystem::current_path() << "\n";
+    std::cout << "Tentando abrir arquivo: " << path << "\n";
+
+    std::ifstream file(path, std::ios::in);
+    if (!file.is_open()) {
+        std::perror("Erro ao abrir o arquivo");
+        return 1;
+    }
+
+    std::cout << "Arquivo aberto com sucesso!\n";
+
+    std::string linha;
+    while (std::getline(file, linha)) {
+        std::cout << "Linha lida: " << linha << "\n";
+    }
+
+    file.close();
     return 0;
 }
